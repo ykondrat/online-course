6d7dbed036d6ee48797b6185f9595bbc1c5fb9ce
Yevhen Kondratyev <yevhenkondratyev@Yevhens-MacBook-Pro.local>
2019-06-30 22:01:22 +0300

    add auth middleware/start winston

--- source/routers/auth/index.js
+++ source/routers/auth/index.js
@@ -3,10 +3,11 @@ import express from 'express';
 
 // Instruments
 import { postLogin, postLogout } from './route';
+import { authorization } from '../../utils';
 
 export const router = express.Router();
 
 router.post('/login', postLogin);
-router.post('/logout', postLogout);
+router.post('/logout', [ authorization(process.env.PASSWORD) ], postLogout);
 
 export { router as auth };
