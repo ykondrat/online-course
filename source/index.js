e2cf0cb4d67dd91f9b1a22eab30dffaa18bbbda0
Yevhen Kondratyev <yevhenkondratyev@Yevhens-MacBook-Pro.local>
2019-07-07 20:16:51 +0300

    add winston logger

--- source/index.js
+++ source/index.js
@@ -6,10 +6,15 @@ import { getPort } from './utils';
 
 // Routers
 import { auth, users, classes, lessons } from './routers';
+import { logger } from './utils';
 
 const PORT = getPort();
 
 app.use(express.json({ limit: '10kb' }));
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(logger);
+}
 
 app.use('/', auth);
 app.use('/users', users);
--- source/utils/index.js
+++ source/utils/index.js
@@ -2,3 +2,4 @@ export { getPort } from './env';
 export { limiter } from './limiter';
 export { validator } from './validator';
 export { authorization } from './authorization';
+export { logger } from './logger';
